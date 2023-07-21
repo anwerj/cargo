59eb5fe17136b79f7e812b5ba37814b93f1e65fd
Junaid Anwer <junaid.anwer@razorpay.com>
2023-07-21 23:27:16 +0530

    [Adding more code]

--- .github/actions/push/push.py
+++ .github/actions/push/push.py
@@ -19,6 +19,19 @@ def push(data):
   except Exception as e:
     print ("Exception: " + str(e))
 
+
+def remove_keys_with_suffix(data, suffixes):
+    if isinstance(data, dict):
+        return {
+            key: remove_keys_with_suffix(value, suffixes)
+            for key, value in data.items()
+            if not any(key.endswith(suffix) for suffix in suffixes)
+        }
+    elif isinstance(data, list):
+        return [remove_keys_with_suffix(item, suffixes) for item in data]
+    else:
+        return data
+
 def main():
   event_file = os.getenv("GITHUB_EVENT_PATH")
   try:
@@ -29,7 +42,7 @@ def main():
     ## Don't want to block pipelines because of failure in this script
     sys.exit(0)
 
-  data = json.load(fd)
+  data = remove_keys_with_suffix(json.load(fd),  ["_url", "_links", "url", "repository", "repo", "sender"])
 
   # Update the watchtower with the same content as the file has
   push(data)
